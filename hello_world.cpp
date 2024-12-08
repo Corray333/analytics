af264b417962794f44d38389d3815d3841f0b227
Mark <mark.corray.off@gmail.com>
2024-12-08 17:47:06 +0300

    style: change style using clang-format to Mozilla

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 
-int main(){
-    // Объявляем переменную для имени пользователя
-    std::string name;
-    std::cout << "Enter your name: ";
-    // Запрашиваем имя пользователя
-    std::cin >> name;
-    // Выводим сообщение с именем пользователя
-    std::cout << "Hello, " + name + "!" << std::endl;
-    return 0;
+int
+main()
+{
+  // Объявляем переменную для имени пользователя
+  std::string name;
+  std::cout << "Enter your name: ";
+  // Запрашиваем имя пользователя
+  std::cin >> name;
+  // Выводим сообщение с именем пользователя
+  std::cout << "Hello, " + name + "!" << std::endl;
+  return 0;
 }
